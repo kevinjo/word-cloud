bf81ea088c7b310860fc8e4b6f2d775794d44bf1
Jleagle <jimeagle@gmail.com>
2014-09-22 23:10:53 +0100

    Sort words and display them

--- src/WordCloud.php
+++ src/WordCloud.php
@@ -5,7 +5,7 @@ class WordCloud
 {
 
   private $words = [];
-  private $range = 10;
+  private $range = 1;
 
   public function __construct()
   {
@@ -20,7 +20,7 @@ class WordCloud
 
   public function addWordsByArray($array)
   {
-    if($this->_isAssociative($array))
+    if(is_assoc($array))
     {
       foreach($array as $word => $data)
       {
@@ -40,7 +40,7 @@ class WordCloud
 
   public function addWord($word, array $data = [], $count = 1)
   {
-    if (isset($this->words[$word]))
+    if(isset($this->words[$word]))
     {
       $this->words[$word]['count'] = $this->words[$word]['count'] + $count;
       foreach($data as $k => $v)
@@ -59,25 +59,102 @@ class WordCloud
     return $this;
   }
 
-  public function getWords()
+  public function getWords($orderBy = 'word', $orderDir = 'asc')
   {
+    switch($orderBy)
+    {
+      case 'count':
+        $this->_calculateRange();
+        $this->_sortByCount($orderDir);
+        break;
+      case 'rand':
+        $this->_sortByRand();
+        break;
+      default:
+        $this->_sortByWord($orderDir);
+    }
+
     return $this->words;
   }
 
-  public function render(
-    $smallest, $largest, $orderBy = 'word', $order = 'asc', $units = 'px'
-  )
+  public function render($fontMin, $fontMax, $fontUnits = 'px', $orderBy = 'word', $orderDir = 'asc')
   {
-    $range = $largest - $smallest;
-    //http://codex.wordpress.org/Function_Reference/wp_tag_cloud
-    // devide by biggest and times by range
+    $fontRange = $fontMax - $fontMin;
+    $words = $this->getWords($orderBy, $orderDir);
 
-    // order by size, name, rand, added-time
-    return 'x';
+    $return = [];
+    foreach($words as $word => $array)
+    {
+      $font = round($array['count'] / $this->range * $fontRange, 2) + $fontMin;
+      $return[] = new Dom('span', ['style' => 'font-size: '.$font.$fontUnits], [$word]);
+    }
+    return implode('', $return);
   }
 
-  private function _isAssociative($array)
+  private function _sortByWord($orderDir = 'asc')
   {
-    return (bool)count(array_filter(array_keys($array), 'is_string'));
+    $orderDir = $orderDir == 'asc' ? 'ksort' : 'krsort';
+    $orderDir($this->words);
+
+    return $this;
   }
+
+  private function _sortByCount($orderDir = 'desc')
+  {
+    uasort(
+      $this->words,
+      function ($a, $b) use ($orderDir)
+      {
+        $a = $a['count'];
+        $b = $b['count'];
+        if($a == $b)
+        {
+          return 0;
+        }
+        if ($orderDir == 'asc')
+        {
+          return ($a < $b) ? -1 : 1;
+        }
+        return ($a > $b) ? -1 : 1;
+      }
+    );
+
+    return $this;
+  }
+
+  private function _sortByRand()
+  {
+    shuffle_assoc($this->words);
+
+    return $this;
+  }
+
+  private function _calculateRange()
+  {
+    $count_min = $count_max = null;
+    foreach($this->words as $word => $array)
+    {
+      if(isset($count_max))
+      {
+        $count_max = max($count_max, $array['count']);
+      }
+      else
+      {
+        $count_max = $array['count'];
+      }
+
+      if(isset($count_min))
+      {
+        $count_min = min($count_min, $array['count']);
+      }
+      else
+      {
+        $count_min = $array['count'];
+      }
+    }
+    $this->range = $count_max - $count_min;
+
+    return $this;
+  }
+
 }
